dde1ecd2e375a4392bf2ecd1be4babab90adefe5
Dvir Edri <dviredri433@gmail.com>
2017-06-26 11:46:23 +0300

    second engine changes

--- jet.js
+++ jet.js
@@ -1,10 +1,14 @@
 var Jet = function (color, size) {
-  this.color = color;
-  this.size = size;
+    this.color = color;
+    this.size = size;
 
-  this.takeOff = function () {
-    console.log('lift off!');
-  }
+    this.takeOff = function () {
+        console.log('lift off!');
+    }
 }
 
 module.exports = Jet;
+module.exports = Rocket;
+this.engageSecondaryEngines = function () {
+    console.log('Secondary Engines Engaged!');
+};
